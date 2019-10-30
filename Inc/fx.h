fac38174ec600526f2119578e6c439dc7b5c3fda
LongerHV <michal@mieszczak.com.pl>
2019-10-30 17:29:23 +0100

    Created structures for LFO and Modulation

--- Inc/fx.h
+++ Inc/fx.h
@@ -11,6 +11,11 @@ extern "C"
 #include "math.h"
 
 // Defines
+#define LFO_SIN 0;
+#define LFO_SQUARE 1;
+#define LFO_TRIANGLE 2;
+#define LFO_SAW_RISING 3;
+#define LFO_SAW_FALLING 4;
 
 // Instance structures
 typedef struct{
@@ -22,12 +27,32 @@ typedef struct{
     float32_t *pData;
 } FX_DelayTypeDef;
 
+typedef struct{
+    uint32_t Waveform;
+    float32_t Frequency;
+    float32_t Depth;
+    float32_t Angle;
+    float32_t Amplitude;
+} FX_LfoTypeDef;
+
+typedef struct{
+    FX_DelayTypeDef *Delay;
+    FX_LfoTypeDef *LFO;
+    float32_t DryLevel;
+    float32_t WetLevel;
+    uint32_t ModType;
+} FX_ModulationTypeDef;
+
 // Function prototypes
+// Data conversion
 void fx_uint16_to_float(uint16_t *pSrc, float32_t *pDst, uint32_t blockSize);
 void fx_float_to_uint16(float32_t *pSrc, uint16_t *pDst, uint32_t blockSize);
 
+// Delay functions
 void fx_delay_init(FX_DelayTypeDef *delay, uint32_t size, uint32_t offset, float32_t feedback, float32_t wet, float32_t *pData);
 void fx_delay(FX_DelayTypeDef *delay, float32_t *pSrc, uint32_t blockSize);
+
+// Modulation functions
 
 #ifdef __cplusplus
 }
