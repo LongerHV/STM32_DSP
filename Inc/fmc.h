db78eb066054b1d97659fa2caf89d4212b061daf
LongerHV <michal@mieszczak.com.pl>
2019-11-25 14:13:23 +0100

    Changed refresh time for sdram. Need more testing

--- Inc/fmc.h
+++ Inc/fmc.h
@@ -36,19 +36,29 @@ extern SDRAM_HandleTypeDef hsdram1;
 #define SDRAM_BANK_ADDR                 ((uint32_t)0xD0000000)
 #define SDRAM_MEMORY_WIDTH               FMC_SDRAM_MEM_BUS_WIDTH_8
 #define SDRAM_TIMEOUT                    ((uint32_t)0xFFFF)
-#define REFRESH_COUNT                    ((uint32_t)0x0603)   /* SDRAM refresh counter */
-  
+#define REFRESH_COUNT                    ((uint32_t)0x2000)   /* SDRAM refresh counter */
+
+// http://www-micro.deis.unibo.it/~romani/Dida01/lezioni/sdram_operation_timing.pdf
+// Test mode
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+
+// Write burst length
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+
+// CAS latency
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+
+// Burst type
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+
+// Burst length 
 #define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
 #define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
 #define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
-#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0003)
 
 /* USER CODE END Private defines */
 
--- src/main.cpp
+++ src/main.cpp
@@ -143,26 +143,29 @@ int main(void)
 
 
   // SDRAM initialization sequence
-  // SDRAM_InitSequence(&hsdram1);
+  SDRAM_InitSequence(&hsdram1);
 
-  // // TESTING FMC
-  // // uint32_t *pSdram = (uint32_t *) 0xD0000000;
-  // uint32_t pSdram = 0xD0000800;
-  // uint8_t Src[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  // uint8_t Dst[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  // // HAL_SDRAM_Write_8b(&hsdram1, pSdram, Src, 10);
-  // for(uint8_t i = 0; i < 10; i++){
-  //   *(__IO uint8_t *) (pSdram + i) = Src[i];
-  // }
-  // HAL_Delay(100);
-  // // HAL_SDRAM_Read_8b(&hsdram1, pSdram, Dst, 10);
-  // for(uint8_t i = 0; i < 10; i++){
-  //   Dst[i] = *(__IO uint8_t *) (pSdram + i);
-  // }
+  // TESTING FMC
+  // uint32_t *pSdram = (uint32_t *) 0xD0000000;
+  uint32_t pSdram = 0xD0000800;
+  uint8_t Src[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+  uint8_t Dst[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  // HAL_SDRAM_Write_8b(&hsdram1, pSdram, Src, 10);
+  for(uint8_t i = 0; i < 10; i++){
+    *(__IO uint8_t *) (pSdram + i) = Src[i];
+  }
+  HAL_Delay(100);
+  // HAL_SDRAM_Read_8b(&hsdram1, pSdram, Dst, 10);
+  for(uint8_t i = 0; i < 10; i++){
+    Dst[i] = *(__IO uint8_t *) (pSdram + i);
+  }
 
   // TESTING LCD SPI
   Display *my_disp = new Display(128, 160, &hspi1, &character_buffer[0]);
   my_disp->FillScreen(BLACK);
+  for(uint8_t i = 0; i < 10; i++){
+    my_disp->DrawChar(0, i, Dst[i], 0xFFFF, 0x0000);
+  }
 
   // Initializing effects
   Effect *effects[10];
