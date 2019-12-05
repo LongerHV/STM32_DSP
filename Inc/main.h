112954486025eb1fd3aca31f43cde6af212cce55
LongerHV <michal@mieszczak.com.pl>
2019-12-05 14:07:51 +0100

    Commented unnecessary includes

--- Inc/main.h
+++ Inc/main.h
@@ -29,7 +29,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32h7xx_hal.h"
-#include "stm32h7xx_hal.h"
+// #include "stm32h7xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -37,7 +37,7 @@ extern "C" {
 #include "arm_const_structs.h"
 #include <stdlib.h>
 #include "mymath.h"
-#include "stm32h743xx.h"
+// #include "stm32h743xx.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
