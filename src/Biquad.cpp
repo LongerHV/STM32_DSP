f31c2a4b92bcb18ce729ac5173325b07a494c07f
LongerHV <michal@mieszczak.com.pl>
2019-12-09 18:00:55 +0100

    Improvements on biquad filter

--- src/Biquad.cpp
+++ src/Biquad.cpp
@@ -6,17 +6,8 @@ Biquad::Biquad(const char *name) {
     this->filter_instance_L = new arm_biquad_cascade_df2T_instance_f32;
     this->filter_instance_R = new arm_biquad_cascade_df2T_instance_f32;
 
-    // arm_biquad_cascade_df1_init_f32(this->filter_instance_L, 1, new float32_t[5], new float32_t[4]);
-    // arm_biquad_cascade_df1_init_f32(this->filter_instance_R, 1, new float32_t[5], new float32_t[4]);
-
-    this->filter_instance_L->numStages = 1;
-    this->filter_instance_R->numStages = 1;
-    this->filter_instance_L->pCoeffs = new float32_t[5];
-    this->filter_instance_R->pCoeffs = new float32_t[5];
-    this->filter_instance_L->pState = new float32_t[2];
-    this->filter_instance_R->pState = new float32_t[2];
-    arm_fill_f32(0.0, this->filter_instance_L->pState, 2);
-    arm_fill_f32(0.0, this->filter_instance_R->pState, 2);
+    arm_biquad_cascade_df2T_init_f32(this->filter_instance_L, 1, new float32_t[5], new float32_t[2]);
+    arm_biquad_cascade_df2T_init_f32(this->filter_instance_R, 1, new float32_t[5], new float32_t[2]);
 
     this->number_of_parameters = 4;
     this->current_parameter = 0;
@@ -24,9 +15,9 @@ Biquad::Biquad(const char *name) {
     this->type = 0;
 
     this->parameters[0] = new Parameter("Type", " ", 0, 6, 1, 0);
-    this->parameters[1] = new Parameter("Frequency", "Hz", 100, 20000, 100, 10000);
-    this->parameters[2] = new Parameter("Q", "", 0, 5000, 100, 1000);
-    this->parameters[3] = new Parameter("Peak gain", "", 1000, 2000, 100, 1000);
+    this->parameters[1] = new Parameter("Frequency", "Hz", 100, 20000, 100, 5000);
+    this->parameters[2] = new Parameter("Q", "", 100, 5000, 100, 1000);
+    this->parameters[3] = new Parameter("Peak gain", "", -10000, 10000, 500, 0);
     for (uint8_t i = 4; i < 10; i++) parameters[i] = NULL;
 
     this->UpdateParameters();
@@ -37,9 +28,9 @@ Biquad::~Biquad() {
 }
 
 void Biquad::RecalculateCoeffitients() {
-    uint8_t type = (uint8_t)this->type;
-    float32_t K = arm_tan(PI * this->Fc / this->Fs);
-    float32_t V;
+    uint8_t type = this->type;
+    float32_t K = arm_tan(PI * (float32_t)this->Fc / (float32_t)this->Fs);
+    float32_t V = 0;
     float32_t norm;
     float32_t a0, a1, a2, b1, b2;
 
@@ -141,19 +132,20 @@ void Biquad::RecalculateCoeffitients() {
             break;
 
         default:
+            a0 = a1 = a2 = b1 = b2 = 0;
             break;
     }
-    this->filter_instance_L->pCoeffs[0] = a0;
-    this->filter_instance_L->pCoeffs[1] = b1;
-    this->filter_instance_L->pCoeffs[2] = b2;
-    this->filter_instance_L->pCoeffs[3] = a1;
-    this->filter_instance_L->pCoeffs[4] = a2;
+    this->filter_instance_L->pCoeffs[0] = -a0;
+    this->filter_instance_L->pCoeffs[1] = -a1;
+    this->filter_instance_L->pCoeffs[2] = -a2;
+    this->filter_instance_L->pCoeffs[3] = -b1;
+    this->filter_instance_L->pCoeffs[4] = -b2;
 
-    this->filter_instance_R->pCoeffs[0] = a0;
-    this->filter_instance_R->pCoeffs[1] = b1;
-    this->filter_instance_R->pCoeffs[2] = b2;
-    this->filter_instance_R->pCoeffs[3] = a1;
-    this->filter_instance_R->pCoeffs[4] = a2;
+    this->filter_instance_R->pCoeffs[0] = -a0;
+    this->filter_instance_R->pCoeffs[1] = -a1;
+    this->filter_instance_R->pCoeffs[2] = -a2;
+    this->filter_instance_R->pCoeffs[3] = -b1;
+    this->filter_instance_R->pCoeffs[4] = -b2;
 }
 
 void Biquad::ProcessBlock(float32_t *pData_left, float32_t *pData_right, uint32_t block_size) {
@@ -164,8 +156,8 @@ void Biquad::ProcessBlock(float32_t *pData_left, float32_t *pData_right, uint32_
 void Biquad::UpdateParameters(){
     this->type = this->parameters[0]->GetValue();
     this->Fc = this->parameters[1]->GetValue();
-    this->Q = this->parameters[2]->GetValue() * 0.001;
-    this->peak_gain = this->parameters[3]->GetValue() * 0.001;
+    this->Q = (float32_t)this->parameters[2]->GetValue() * 0.001;
+    this->peak_gain = (float32_t)this->parameters[3]->GetValue() * 0.001;
 
     this->RecalculateCoeffitients();
 }
